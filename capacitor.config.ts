1422dffbb8deaa8fe7e9d4e711fb80e7d1f71899
Jovanny <menajovanny9@gmail.com>
2026-08-18 16:02:57 -0400

    feat: configure live updates plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Prestamos',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '437f73f4', // Reemplaza con tu App ID real de Appflow
+      channel: 'Production', // El canal desde donde se descargarán las actualizaciones
+      autoUpdateMethod: 'background', // Cómo se aplicarán ('background' o 'kill')
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
